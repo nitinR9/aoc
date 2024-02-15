b70639c347ec8265751a7a8603cd4a1002523b0a
splintercell9dev <splintercell9dev@gmail.com>
2024-02-15 22:10:21 +0530

    2015: completed day3 part2

--- 2015/day3/day3.go
+++ 2015/day3/day3.go
@@ -3,12 +3,18 @@ package main
 import (
 	"2015/common"
 	"fmt"
-	"strings"
 )
 
 type pos struct {
 	x int
 	y int
+}
+
+var posmap = map[string]pos{
+	"^": {x: -1, y: -1},
+	"v": {x: 1, y: 1},
+	">": {x: 1, y: 0},
+	"<": {x: -1, y: 0},
 }
 
 func newPos() pos {
@@ -24,24 +30,19 @@ func (p *pos) setPos(dir pos) {
 	p.y += dir.y
 }
 
-func part1(input []string) {
+func part1(input string) {
 	hashmap := map[string]int{}
-	posmap := map[string]pos{
-		"^": {x: -1, y: -1},
-		"v": {x: 1, y: 1},
-		">": {x: 1, y: 0},
-		"<": {x: -1, y: 0},
-	}
+
 	curr_pos := newPos()
 	count := 0
 	for _, val := range input {
 		string := ""
 
-		if val == "^" {
+		if val == '^' {
 			curr_pos.setPos(posmap["^"])
-		} else if val == "v" {
+		} else if val == 'v' {
 			curr_pos.setPos(posmap["v"])
-		} else if val == ">" {
+		} else if val == '>' {
 			curr_pos.setPos(posmap[">"])
 		} else {
 			curr_pos.setPos(posmap["<"])
@@ -53,11 +54,46 @@ func part1(input []string) {
 		}
 	}
 
-	fmt.Println("Total houses delivered =", count)
+	fmt.Println("Part1:", count)
+}
+
+func (p *pos) move(h map[string]int, char rune) int {
+	if char == '^' {
+		p.setPos(posmap["^"])
+	} else if char == 'v' {
+		p.setPos(posmap["v"])
+	} else if char == '>' {
+		p.setPos(posmap[">"])
+	} else {
+		p.setPos(posmap["<"])
+	}
+	string := fmt.Sprintf("%+v", p)
+	if _, ok := h[string]; !ok {
+		h[string] = 1
+		return 1
+	}
+	return 0
+}
+
+func part2(input string) {
+	hash := map[string]int{}
+
+	santa := newPos()
+	robot := newPos()
+	count := 0
+	for index, char := range input {
+		if index%2 == 0 {
+			count += santa.move(hash, char)
+		} else {
+			count += robot.move(hash, char)
+		}
+	}
+
+	fmt.Println("Part2:", count)
 }
 
 func main() {
-	common.GetFile("input.txt")
-	result := strings.Split(common.Input, "")
-	part1(result)
+	common.GetFile("test.txt")
+	part1(common.Input)
+	part2(common.Input)
 }
