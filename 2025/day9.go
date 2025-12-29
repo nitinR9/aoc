a80efb9a5eea6cbd384c2cc88d68a6b98303aec2
nitinR9 <splintercell9dev@gmail.com>
2025-12-29 13:01:41 +0530

    day 9 p 2: check rec outside fn 97% less and fill grid area with go routines 90% less time

--- 2025/day9.go
+++ 2025/day9.go
@@ -6,6 +6,7 @@ import (
 	"slices"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 type Vec2d struct {
@@ -13,12 +14,16 @@ type Vec2d struct {
 	y int
 }
 
-type ByteSlice []byte
+type BitSet struct {
+	rows int
+	cols int
+	grid []uint64
+}
 
 func Area(p1 Vec2d, p2 Vec2d) int64 {
 	width := int64(math.Abs(float64(p2.x-p1.x))) + 1
 	height := int64(math.Abs(float64(p2.y-p1.y))) + 1
-
+	
 	return width * height
 }
 
@@ -30,83 +35,128 @@ func Min(nums ...int) int {
 	return slices.Min(nums)
 }
 
-func (b *ByteSlice) setPixel(width, r, c int) {
-	idx := (r * width) + c
-	byteIdx := idx/8
-	bitIdx := idx % 8
-	(*b)[byteIdx] |= 1 << bitIdx
+func NewBitSet(rows, cols int) *BitSet {
+	totalBits := rows * cols
+	totalSetSize := (totalBits+63) / 64
+	return &BitSet{
+		rows: rows,
+		cols: cols,
+		grid: make([]uint64, totalSetSize),
+	}
 }
 
-func (b *ByteSlice) hasPixel(width, r, c int) bool {
-	idx := (r * width) + c
-	byteIdx := idx/8
-	bitIdx := idx % 8
-
-	return (*b)[byteIdx] & (1 << bitIdx) != 0
+func (b *BitSet) hasTile(r, c int) bool {
+	idx := (r * b.cols) + c
+	return (b.grid[idx >> 6] & uint64(1 << (idx & 63))) != 0
 }
 
-func (b *ByteSlice) createWall(width int, p1, p2 Vec2d) {
+func (b *BitSet) addTilesToGrid(p1, p2 Vec2d) {
+	start, end := 0, 0
 	if p1.x == p2.x {
-		start, end := 0, 0
-		if p1.y < p2.y {
-			start, end = p1.y, p2.y
-		} else {
-			start, end = p2.y, p1.y
+		start, end = p1.y, p2.y
+		if start > end {
+			start, end = end, start
 		}
-		for j:=start; j<=end; j++ {
-			b.setPixel(width, j, p1.x)
+		for y := start; y <= end; y++ {
+			idx := (y * b.cols) + p1.x
+			b.grid[idx >> 6] |= uint64(1 << (idx & 63))
 		}
-	} else if p2.y == p1.y {
-		start, end := 0, 0
-		if p1.x < p2.x {
-			start, end = p1.x, p2.x
-		} else {
-			start, end = p2.x, p1.x
+	} else if p1.y == p2.y {
+		start, end = p1.x, p2.x
+		if start > end {
+			start, end = end, start
 		}
-		for i:=start; i<=end; i++ {
-			b.setPixel(width, p1.y, i)
+		for x := start; x <= end; x++ {
+			idx := (p1.y * b.cols) + x
+			b.grid[idx >> 6] |= uint64(1 << (idx & 63))
 		}
 	}
 }
 
-func (b *ByteSlice) fillWithWalls(width, height int) {
-	for r:=0; r<height; r++ {
-		foundIdx := -1
-		for c := 0; c < width; c++ {
-			if b.hasPixel(width, r,c) {
-				if foundIdx != -1 {
-					if (c - foundIdx > 1) {
-						b.createWall(width, Vec2d{foundIdx+1,r}, Vec2d{c-1, r})
-					}
-				}
-				foundIdx = c
+func (b *BitSet) processRow(row int) {
+	cS, cE := 0, b.cols-1
+	found := false
+	for cS <= cE {
+		fS, fE := b.hasTile(row,cS), b.hasTile(row,cE)
+		if !found {
+			if fS && fE {
+				found = true
+			} else if !fS {
+				cS++
+			} else if !fE {
+				cE--
 			}
+		} else {
+			idx := 0
+			if cS != cE {
+				idx = (row * b.cols) + cS
+				b.grid[idx >> 6] |= uint64(1 << (idx & 63))
+				idx = (row * b.cols) + cE
+				b.grid[idx >> 6] |= uint64(1 << (idx & 63))
+			} else {
+				idx = (row * b.cols) + cS
+				b.grid[idx >> 6] |= uint64(1 << (idx & 63))
+			}
+			cS++
+			cE--
 		}
 	}
 }
 
-func (b *ByteSlice) isOutsideBorders(width int, p1, p2 Vec2d) bool {
-	if p1.x == p2.x {
-		start, end := 0, 0
-		if p1.y < p2.y {
-			start, end = p1.y, p2.y
+func (b *BitSet) fillWithTilesMultithreaded() {
+	var wg sync.WaitGroup
+	rS, rE := 0, b.rows-1
+
+	for rS <= rE {
+		if rS == rE {
+			wg.Add(1)
+			go func(row int) {
+				defer wg.Done()
+				b.processRow(row)
+			}(rS)
 		} else {
-			start, end = p2.y, p1.y
+			wg.Add(2)
+			go func(row int) {
+				defer wg.Done()
+				b.processRow(row)
+			}(rS)
+			go func(row int) {
+				defer wg.Done()
+				b.processRow(row)
+			}(rE)
 		}
-		for j:=start; j<=end; j++ {
-			if !b.hasPixel(width, j, p1.x) {
+		rS++
+		rE--
+	}
+	wg.Wait()
+}
+
+func (b *BitSet) fillWithTilesSingleThreaded() {
+	rS, rE := 0, b.rows-1
+	for rS <= rE {
+		b.processRow(rS)
+		rS++
+	}	
+}
+
+func (b *BitSet) isOutsideBorders(p1, p2 Vec2d) bool {
+	if p1.x == p2.x {
+		start, end := p1.y, p2.y
+		if start > end {
+			start, end = end, start
+		}
+		for y := start; y <= end; y++ {
+			if !b.hasTile(y, p1.x) {
 				return true
 			}
 		}
-	} else if p2.y == p1.y {
-		start, end := 0, 0
-		if p1.x < p2.x {
-			start, end = p1.x, p2.x
-		} else {
-			start, end = p2.x, p1.x
+	} else if p1.y == p2.y {
+		start, end := p1.x, p2.x
+		if start > end {
+			start, end = end, start
 		}
-		for i:=start; i<=end; i++ {
-			if !b.hasPixel(width, p1.y, i) {
+		for x := start; x <= end; x++ {
+			if !b.hasTile(p1.y, x) {
 				return true
 			}
 		}
@@ -114,18 +164,21 @@ func (b *ByteSlice) isOutsideBorders(width int, p1, p2 Vec2d) bool {
 	return false
 }
 
-func (b *ByteSlice) checkIfRecOutside(width int, p1, p2 Vec2d) bool {
-	
+func (b *BitSet) isRecOutside(p1, p2 Vec2d) bool {
 	if p1.x == p2.x || p1.y == p2.y {
-		return b.isOutsideBorders(width, p1, p2)
+		return b.isOutsideBorders(p1, p2)
 	}
 
-	p3, p4 := Vec2d{p1.x, p2.y}, Vec2d{p2.x, p1.y}
+	points := []Vec2d{p1, {p1.x, p2.y}, p2, {p2.x, p1.y}, p1}
 
-	points := []Vec2d{p1,p3,p2,p4,p1}
+	// check if corners are outside grid of tiles (speed boost)
+	if !b.hasTile(p2.y, p1.x) || !b.hasTile(p1.y, p2.x) {
+		return true
+	}
 
-	for i :=0; i<len(points)-1; i++ {
-		if b.isOutsideBorders(width, points[i], points[i+1]) {
+	// check all four borders if not outside grid of tiles
+	for i:=0; i<len(points)-1; i++ {
+		if b.isOutsideBorders(points[i], points[i+1]) {
 			return true
 		}
 	}
@@ -133,27 +186,16 @@ func (b *ByteSlice) checkIfRecOutside(width int, p1, p2 Vec2d) bool {
 	return false
 }
 
-func part2day9(grid []Vec2d, dim int) int64 {
-	totalBits := dim * dim
-	totalBytes := (totalBits + 7) / 8
-	byteSet := make(ByteSlice, totalBytes)
-
-	for i := 0; i<len(grid)-1; i++ {
-		p1, p2 := grid[i], grid[i+1]
-		byteSet.createWall(dim, p1, p2)
-	}
-	byteSet.createWall(dim, grid[len(grid)-1], grid[0])
-
-	byteSet.fillWithWalls(dim, dim)
-
+func (b *BitSet) getMaxAreaRectangle(grid *[]Vec2d) int64 {
 	maxArea := int64(0)
-	
-	for i := 0; i<len(grid)-1; i++ {
-		for j := 0; j < len(grid); j++ {
-			if !byteSet.checkIfRecOutside(dim, grid[i], grid[j]){
-				result := Area(grid[i], grid[j])
-				if result > maxArea {
-					maxArea = result
+
+	for i := 0; i < len(*grid)-1; i++ {
+		for j := 0; j < len(*grid); j++ {
+			g1, g2 := (*grid)[i], (*grid)[j]
+			if !b.isRecOutside(g1, g2) {
+				a := Area(g1, g2)
+				if a > maxArea {
+					maxArea = a
 				}
 			}
 		}
@@ -162,36 +204,81 @@ func part2day9(grid []Vec2d, dim int) int64 {
 	return maxArea
 }
 
-func part1day9(grid []Vec2d) (int64, int) {
-	max, maxDim := int64(0), 0
+func part2day9Slow(grid []Vec2d, dim Vec2d) int64 {
+	bs := NewBitSet(dim.y+1, dim.x+1)
+
+	for i:=0; i<len(grid)-1; i++ {
+		p1, p2 := grid[i], grid[i+1]
+		bs.addTilesToGrid(p1, p2)
+	}
+
+	bs.addTilesToGrid(grid[len(grid)-1], grid[0])
+
+	bs.fillWithTilesSingleThreaded()
+
+	maxArea := int64(0)
+	for i := 0; i < len(grid)-1; i++ {
+		for j := 0; j < len(grid); j++ {
+			if !bs.isRecOutside(grid[i], grid[j]) {
+				a := Area(grid[i], grid[j])
+				if a > maxArea {
+					maxArea = a
+				}
+			}
+		}
+	}
+
+	return maxArea
+}
+
+func part2day9Fast(grid []Vec2d, dMax Vec2d) int64 {
+
+	bs := NewBitSet(dMax.y+1, dMax.x+1)
+
+	for i:=0; i<len(grid)-1; i++ {
+		p1, p2 := grid[i], grid[i+1]
+		bs.addTilesToGrid(p1, p2)
+	}
+
+	bs.addTilesToGrid(grid[len(grid)-1], grid[0])
+
+	bs.fillWithTilesMultithreaded()
+
+	maxArea := bs.getMaxAreaRectangle(&grid)
+	
+	return maxArea
+}
+
+func part1day9(grid []Vec2d) (int64, Vec2d) {
+	max, pMax := int64(0), Vec2d{0,0}
 	for i := 0; i < len(grid)-1; i++ {
 		for j := i + 1; j < len(grid); j++ {
 			result := Area(grid[i], grid[j])
 			if result > max {
 				max = result
 			}
-
-			maxDim = Max(maxDim, grid[i].x, grid[i].y, grid[j].x, grid[j].y)
+			
+			pMax = Vec2d{x: Max(grid[i].x, grid[j].x, pMax.x), y: Max(grid[i].y, grid[j].y, pMax.y)}
 		}
 	}
-	return max, maxDim
+	return max, pMax
 }
-
+						
 func day9() {
 	data := readFile("inputs/day9.txt", "\r\n")
 	grid := []Vec2d{}
-
+	
 	for _, line := range data {
 		split := strings.Split(line, ",")
 		xpos, _ := strconv.Atoi(split[0])
 		ypos, _ := strconv.Atoi(split[1])
 		grid = append(grid, Vec2d{x: xpos, y: ypos})
 	}
-
-
-	p1, charDim := part1day9(grid)
+	
+	p1, pMax := part1day9(grid)
 	fmt.Println("Day 9 part 1:", p1)
-	p2 := part2day9(grid, charDim+1)
+
+	p2 := part2day9Fast(grid, pMax)
 	fmt.Println("Day 9 part 2:", p2)
 	fmt.Println("--------------------------")
 }
